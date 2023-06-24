8d01cd80dfa7c04b3a4a843640445d67b52d50cf
Frodecas <fetcher2009@yahoo.com>
2023-06-23 18:09:58 -0700

    project with error

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_diagonal - printing lines in diagonal
+* @n: input variable
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+_putchar('\n');
+for (i = 0; j < 1; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
